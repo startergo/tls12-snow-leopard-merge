4c0b3cb4fad8c9d9d6a1022d094656f70b83b89c
startergo <7897244+startergo@users.noreply.github.com>
2026-06-17 19:04:38 -0400

    Add README

--- scripts/Safari_tls12_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export DYLD_INSERT_LIBRARIES=/usr/local/lib/libsecurity_ssl_tls12.dylib
-exec arch -x86_64 /Applications/Safari.app/Contents/MacOS/Safari.real "$@"
--- sources/algtest.c
+++
@@ -1,9 +0,0 @@
-#include <Security/cssm.h>
-#include <stdio.h>
-int main(void) {
-    printf("CSSM_ALGID_SHA256WithRSA=%u\n", (unsigned)CSSM_ALGID_SHA256WithRSA);
-    printf("CSSM_ALGID_SHA1WithRSA=%u\n",   (unsigned)CSSM_ALGID_SHA1WithRSA);
-    printf("CSSM_ALGID_RSA=%u\n",            (unsigned)CSSM_ALGID_RSA);
-    printf("CSSM_ALGID_NONE=%u\n",           (unsigned)CSSM_ALGID_NONE);
-    return 0;
-}
